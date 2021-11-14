173c2947e8165263ff901f21e841cbf2b1cfd2d6
Dayna Brown <dkb308@nyu.edu>
2021-11-14 06:20:57 -0600

    Hello CCLab

---
+++ Week 10.2/js/script.js
@@ -0,0 +1,12 @@
+
+
+function change() {
+  x+= 10
+  document.getElementById('box');
+  conaolw.log(b);
+  b.innerHTML = "WOW!"
+  //b.style.width = "200px";
+  //b.style.height = "50px";
+  b.style.left = x+ "px";
+  b.style.backgroundColor = "Red";
+}
